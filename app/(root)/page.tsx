ca711af928aae9b1002b037a15d5662864715ee6
Rukky <abrukevwe@gmail.com>
2025-02-17 16:24:52 +0100

    Begin

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,18 @@
+import SearchForm from "../components/SearchForm";
 
+export default async function Home({ searchParams }: {
+  searchParams: Promise<{query?: string}>
+}) {
 
-export default function Home() {
+  const query = (await searchParams).query
+
   return (
-   <>
-    <h1>Home</h1>
-   </>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startup <br /> Conneect With Entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions </p>
+        <SearchForm query={query}/>
+      </section>
+    </>
   );
 }
